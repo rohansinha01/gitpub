d675aef43db5c76601503377dda508fea995f6b3
rohansinha01 <rohansinha01@gmail.com>
2023-12-14 20:39:52 -0500

    added css

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express")
-const app = express()
 const drinks = require("./models/drinks.js")
+const app = express()
+
+
+app.use(express.static("public"))
 
 app.get('/', (req,res) => {
     res.send('Welcome to the Gitpub App!')
 })
 
 app.get('/drinks', (req,res) => {
-    res.send(drinks)
+    res.render("index.ejs", {drinks})
 })
+
 app.listen(3000, () => {console.log("Welcome to the Gitpub App!")})
