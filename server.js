f868df3c89e05e6aa5001feff60d913fad879670
rohansinha01 <rohansinha01@gmail.com>
2023-12-14 19:43:48 -0500

    added json drinks

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express()
+const drinks = require("./models/drinks.js")
 
+app.get('/', (req,res) => {
+    res.send('Welcome to the Gitpub App!')
+})
 
-
-
+app.get('/drinks', (req,res) => {
+    res.send(drinks)
+})
 app.listen(3000, () => {console.log("Welcome to the Gitpub App!")})
