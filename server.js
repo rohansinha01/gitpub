5b88744b437a0f9a955c8a13572dbc162cffc1cc
rohansinha01 <rohansinha01@gmail.com>
2023-12-20 14:22:34 -0500

    comment added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 
 app.use(express.static("public"))
-
+//Is there a way to add styling to this first page "/"? I can't seem to get anything outside of hyperlinking it.
 app.get('/', (req,res) => {
     res.send(`<h1><a href="/drinks">'Welcome to the Gitpub App!'</a></h1>`)
     
